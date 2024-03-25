ba0701a57a0fcab28c290158c9948be41464f706
Amr Adel <amr96.adel@gmail.com>
2024-03-25 23:23:26 +0200

    v1.2

--- model/user.py
+++ model/user.py
@@ -3,13 +3,14 @@
 from sqlalchemy import create_engine, Column, String
 from base_model import BaseModel, Base
 
-
 class User(BaseModel, Base):
     __tablename__ = 'users'
     id = Column(String(60), primary_key=True, unique=True)
-    first_name = Column(String(128), primary_key=True, nullable=True)
-    last_name = Column(String(128), nullable=True)
-    email = Column(String(128), nullable=True)
+    first_name = Column(String(128), primary_key=True)
+    last_name = Column(String(128))
+    email = Column(String(128))
+    date = Column(String(128))
+    occupation = Column(String(128))
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
--- web_app/app.py
+++ web_app/app.py
@@ -6,25 +6,17 @@ from model.user import User
 app = Flask(__name__)
 
 
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        data = request.form.to_dict()
+        newUser = User(**data)
+        storage.new(newUser)
+        storage.save()
+        return make_response(jsonify(newUser.to_dict()), 201)
 
-@app.route("/")
-def route():
-    return render_template('newapp.html')
+    return render_template('index.html')
 
-@app.route("/submit", methods=['POST'], strict_slashes=False)
-def new_user():
-    if not request.get_json():
-        abort(400, description="Not a JSON")
-
-    if 'email' not in request.get_json():
-        abort(400, description="Missing email")
-
-    data = request.get_json()
-    newUser = User(**data)
-    storage.new(newUser)
-    storage.save()
-    return make_response(jsonify(newUser.to_dict()), 201)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
