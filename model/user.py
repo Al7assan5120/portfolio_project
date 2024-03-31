d9afa6272e32badf632ab69e6134cf839c6019d8
Amr Adel <amr96.adel@gmail.com>
2024-03-31 20:19:57 +0200

    add tracking api

--- model/user.py
+++ model/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from sqlalchemy import create_engine, Column, String
+from sqlalchemy import create_engine, Column, String, Date, func
 from base_model import BaseModel, Base
 
 class User(BaseModel, Base):
@@ -9,8 +9,10 @@ class User(BaseModel, Base):
     first_name = Column(String(128), primary_key=True)
     last_name = Column(String(128))
     email = Column(String(128))
-    date = Column(String(128))
+    submitDate = Column(Date, default=func.current_date())
+    availableDate = Column(Date)
     occupation = Column(String(128))
+    status = Column(String(128), default="Pending")
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
--- web_app/app.py
+++ web_app/app.py
@@ -10,12 +10,16 @@ app.secret_key = secret_key
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = "---"
+app.config["MAIL_USERNAME"] = "amr96.adel@gmail.com"
 app.config["MAIL_PASSWORD"] = "gswntjlzalbihyjg"
 mail = Mail(app)
 
+@app.route("/", methods=["GET"])
+def idnex():
+    return render_template('index.html')
 
-@app.route("/", methods=["GET", "POST"])
+
+@app.route("/newapp", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         data = request.form.to_dict()
@@ -31,13 +35,31 @@ def index():
                           sender=app.config["MAIL_USERNAME"],
                           recipients=[newUser.email],
                           body=message_body)
-        
+
         mail.send(message)
 
         # Submission Success Message!
-        flash(f"{newUser.first_name}, Your form was subitted successfully, For tracking purposes we've sent id of your form to {newUser.email}!", "success")
+        flash(f"{newUser.first_name}, Your form was submitted successfully, For tracking purposes we've sent id of your form to {newUser.email}!", "success")
 
     return render_template('index.html')
+
+
+
+
+
+@app.route("/tracking", methods=["GET", "POST"])
+def tracking():
+    if request.method == "POST":
+        userId = request.form.get('ID')
+        user = storage.get(userId)
+        print(userId, user)
+        if (user == "None"):
+            print("amr")
+            flash ("Invalid ID", "faild")
+        else:
+            flash (f"{user.first_name}, Your Application current status is : {user.status}", "success")
+
+    return render_template('tracking.html')
 
 
 if __name__ == "__main__":
